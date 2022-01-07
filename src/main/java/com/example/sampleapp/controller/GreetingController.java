a4dcaead80a1db0ca077f23a0bd9cbf6cd2e1018
Akash <akkida746@gmail.com>
2022-01-07 06:13:03 +0530

    Added test service

--- src/main/java/com/example/sampleapp/controller/GreetingController.java
+++ src/main/java/com/example/sampleapp/controller/GreetingController.java
@@ -1,13 +1,38 @@
 package com.example.sampleapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @RestController
 public class GreetingController {
 
+    @Value("${test.url}")
+    private String testUrl;
+
     @GetMapping("/greeting")
-    public String greeting(){
+    public String greeting() {
         return "Greeting";
+    }
+
+    @GetMapping("/test")
+    public String testService() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(testUrl))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+
     }
 }

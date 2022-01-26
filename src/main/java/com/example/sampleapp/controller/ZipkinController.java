69c2a5963f95b3932532604d4a105f005379be05
Akki <akkida746@gmail.com>
2022-01-26 07:00:56 +0000

    refactor

--- src/main/java/com/example/sampleapp/controller/ZipkinController.java
+++ src/main/java/com/example/sampleapp/controller/ZipkinController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.logging.Logger;
 
 @RestController
